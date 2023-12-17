1068c996f1f4e9c230e2859c2fe38fd52a276ce3
Rami Chasygov <3c9wjqgw5@mozmail.com>
2023-12-17 14:54:42 +0300

    Day 4 2/2

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,4 +1,4 @@
-use std::thread::current;
+use std::{collections::HashMap, thread::current};
 
 fn main() -> std::io::Result<()> {
     let input = std::fs::read_to_string("input-1.txt")?;
@@ -6,9 +6,15 @@ fn main() -> std::io::Result<()> {
     let mut cards: Vec<_> = vec![];
 
     input.split('\n').for_each(|l| {
-        if let [_card_num, rest] = l.split(':').collect::<Vec<&str>>().as_slice() {
+        if let [card_num, rest] = l.split(':').collect::<Vec<&str>>().as_slice() {
             if let [winning, having] = rest.split('|').collect::<Vec<&str>>().as_slice() {
                 cards.push(Card {
+                    num: card_num
+                        .split(' ')
+                        .filter(|c| *c != "")
+                        .last()
+                        .map(|c| c.parse::<u32>().expect("invalid card num"))
+                        .expect("always card num"),
                     winning: winning
                         .trim()
                         .split(' ')
@@ -47,11 +53,39 @@ fn main() -> std::io::Result<()> {
 
     eprintln!("sum of points of all the cards: {:?}", sum);
 
+    let mut new_total = 0;
+
+    cards.iter().for_each(|c| {
+        new_total += calc_copies(c, &cards);
+    });
+
+    eprintln!("new rules total: {}", new_total);
+
     Ok(())
+}
+
+fn calc_copies(card: &Card, cards: &Vec<Card>) -> u32 {
+    let count = card
+        .winning
+        .iter()
+        .filter(|n| card.current.contains(n))
+        .count() as u32;
+
+    let mut total = 1;
+
+    //                      1               1     4
+    for sub_id in card.num + 1..=(card.num + count) {
+        if let Some(sub_card) = cards.iter().find(|c| c.num == sub_id) {
+            total += calc_copies(sub_card, cards)
+        }
+    }
+
+    total
 }
 
 #[derive(Debug)]
 struct Card {
+    num: u32,
     winning: Vec<u32>,
     current: Vec<u32>,
 }
